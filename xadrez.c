a64e09e689c4d801b0f340774b2404e43f3a734f
Wuandemberg1 <wuandemberg@gmail.com>
2025-05-06 19:32:31 +0000

    Xadrez novato completo

--- xadrez.c
+++ xadrez.c
@@ -1,21 +1,70 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+int main (){
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+int torre = 0;
+int rainha = 0;
+int bispo = 0;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+//Xadrez
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+printf("Xadrez Game:\n\n\n");
+
+//Mover torre 5 casas para a direita.
+
+printf("Movimentação da torre:\n\n");
+
+for (torre ; torre<5 ; torre++){
+
+    
+    printf("Direita\n");
+
+
+}
+
+//Quebra de linha enre os movimentos da torre e da rainha.
+
+    printf("\n");
+
+//Mover a rainha 8 casas para a esquerda.
+
+
+printf("Movimentação da rainha:\n\n");
+
+while (rainha < 8){
+
+    printf("Esquerda\n");
+
+    rainha++;
+
+
+}
+
+//Quebra de linha entre os movimentos da rainha e o bispo.
+
+    printf("\n");
+
+//Mover o bispo 5 casas na diagonal superior direita
+
+printf("Movimentação do bispo:\n\n");
+
+do {
+
+ 
+    printf("Direita\n");
+    printf("Cima\n");
+
+    bispo++;
+
+
+} while (bispo < 5);
+
+ 
+
+
+
+
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
